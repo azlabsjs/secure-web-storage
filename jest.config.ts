be3afa512dc8e52bd61aae8ff112a989d8366b0c
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2022-02-21 22:52:16 +0000

    code cleanup

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)?$': 'babel-jest'
+    },
+    // testMatch: [
+    //     '<rootDir>/**/*.spec.(js|jsx|ts|tsx)'
+    // ],
+}
--- tests/storage.spec.js
+++ tests/storage.spec.js
@@ -1,10 +1,10 @@
-import { createStorage, InMemoryStorage } from '../src';
+import { createStorage, InMemoryStorage } from '../dist';
 
 const storage = createStorage(new InMemoryStorage(), 'password');
 
 describe('Secure Storage Tests', () => {
   it('Creates an instance of StorageInterface', () => {
-    expect(storage.getInternal()).toBeInstanceOf(Storage);
+    expect(storage.getInternal()).toBeInstanceOf(InMemoryStorage);
   });
   it('should add an item into the storage with the key=key1', () => {
     storage.setItem('key1', { lat: 3.784943, long: 1.76284 });
@@ -12,7 +12,6 @@ describe('Secure Storage Tests', () => {
   });
   it('should hash the key=key1 before adding it to the internal storage', () => {
     storage.setItem('key1', { lat: 3.784943, long: 1.76284 });
-    console.log(storage.getInternal().key(0));
     expect(storage.getInternal().key(0)).not.toEqual('key1');
   });
 });
